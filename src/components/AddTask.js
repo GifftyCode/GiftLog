ca2128c1885e5b579cd47e024b92f518c227a067
GifftyCode <ulokangozi@yahoo.com>
2024-06-30 23:21:36 +0100

    Add & Show Task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,20 @@
-const AddTask = () => {
+const AddTask = ({ taskList, setTaskList }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const date = new Date();
+    const newTask = {
+      id: date.getTime(),
+      name: e.target.task.value,
+      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+    };
+
+    setTaskList([...taskList, newTask]);
+    e.target.task.value = '';
+  };
   return (
     <section className='addTask'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           name='task'
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,23 +1,26 @@
-const ShowTask = () => {
+const ShowTask = ({ taskList, setTaskList }) => {
   return (
     <section className='showTask'>
       <div className='head'>
         <div>
           <span className='title'>Todo</span>
-          <span className='count'>0</span>
+          <span className='count'>{taskList.length}</span>
         </div>
-        <button className='clearAll'>Clear All</button>
+        <button className='clearAll' onClick={() => setTaskList([])}>
+          Clear All
+        </button>
       </div>
       <ul>
-        <li>
-          <p>
-            <span className='name'>Task A</span>
-            <span className='time'>2:09:01 AM 9/14/2030</span>
-          </p>
-
-          <i className='bi bi-pencil-square'></i>
-          <i className='bi bi-trash'></i>
-        </li>
+        {taskList.map((task) => (
+          <li key={task.id}>
+            <p>
+              <span className='name'>{task.name}</span>
+              <span className='time'>{task.time}</span>
+            </p>
+            <i className='bi bi-pencil-square'></i>
+            <i className='bi bi-trash'></i>
+          </li>
+        ))}
       </ul>
     </section>
   );
